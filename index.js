f6349426e599e8b923129ca66fda09bddd1049ae
Jeff Fowler <jeffowler@gmail.com>
2018-02-19 15:35:52 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
+
+const reducer = function(state = 0, action) {
+  console.log(action);
+  if (action.type == 'increment') {
+    state += 1;
+  }
+  return state;
+};
+
+const store = createStore(combineReducers({reducer}), {});
+
+console.log(store);
+
+class Wrapper extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{store.getState().reducer}</h1>
+        <button onClick={() => store.dispatch({type: 'increment'})}>
+          increment
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Wrapper />, document.getElementById('foo'));
